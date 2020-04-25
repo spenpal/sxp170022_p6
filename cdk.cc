de1fba46eab0fc7c22387cfe30c57f989097436a
spenpal2000 <sanjeev.penupala@gmail.com>
2020-04-25 11:04:52 -0500

    Created Multi-File Project

---
+++ cdk.cc
@@ -0,0 +1,67 @@
+/*
+ * Filename:    cdk.cc
+ * Date:        04/25/2020
+ * Author:      Sanjeev Penupala
+ * Email:       sxp170022@utdallas.edu
+ * Course:      CS 3377.501 Spring 2020
+ * Version:     1.0
+ * Copyright    2020, All Rights Reserved
+ *
+ * Description:
+ *
+ *	Display Header Record(s) and Data Record(s) In CDK Matrix
+ *
+ */
+
+#include "program6.h"
+
+void display(string matrix[MATRIX_HEIGHT + 1][MATRIX_WIDTH + 1])
+{
+  WINDOW        *window;
+  CDKSCREEN     *cdkscreen;
+  CDKMATRIX     *myMatrix;           // CDK Screen Matrix
+
+  const char    *rowTitles[MATRIX_HEIGHT+1] =   {"x", "a", "b", "c", "d", "e"};
+  const char    *columnTitles[MATRIX_WIDTH+1] = {"x", "a", "b", "c"};
+  int           boxWidths[MATRIX_WIDTH+1] =     {BOX_WIDTH, BOX_WIDTH, BOX_WIDTH, BOX_WIDTH};
+  int           boxTypes[MATRIX_WIDTH+1] =      {vMIXED, vMIXED, vMIXED, vMIXED};
+
+  //Initialize the CDK screen.
+  window = initscr();
+  cdkscreen = initCDKScreen(window);
+
+  // Start CDK Colors
+  initCDKColor();
+
+  // Create the matrix.  Need to manually cast (const char**) to (char **)
+  myMatrix = newCDKMatrix(cdkscreen, CENTER, CENTER, MATRIX_HEIGHT, MATRIX_WIDTH, MATRIX_HEIGHT, MATRIX_WIDTH,
+                          MATRIX_NAME_STRING, (char **) rowTitles, (char **) columnTitles, boxWidths,
+			  boxTypes, 1, 1, ' ', ROW, true, true, false);
+
+  if(myMatrix == NULL)
+  {
+    printf("Error creating Matrix\n");
+    exit(1);
+  }
+
+  // Display the Matrix
+  drawCDKMatrix(myMatrix, true);
+
+  // Dipslay a message
+  for(int r = 1; r < MATRIX_HEIGHT + 1; r++)
+  {
+    for(int c = 1; c < MATRIX_WIDTH + 1; c++)
+    {
+      setCDKMatrixCell(myMatrix, r, c, matrix[r][c].c_str());
+    }
+  }
+
+  drawCDKMatrix(myMatrix, true);        // REQUIRED
+
+  sleep(10);    // Time Of Output Screen
+
+  // TODO: If Key Is Pressed, Exit CDK Matrix
+  
+
+  endCDK();     // Cleanup Screen
+}
---
+++ program6.h
@@ -0,0 +1,61 @@
+/*
+ * Filename:    program6.h
+ * Date:        04/25/2020
+ * Author:      Sanjeev Penupala
+ * Email:       sxp170022@utdallas.edu
+ * Course:      CS 3377.501 Spring 2020
+ * Version:     1.0
+ * Copyright    2020, All Rights Reserved
+ *
+ * Description:
+ *
+ *	Header File
+ *
+ */
+
+#ifndef PROGRAM6_H
+#define PROGRAM6_H
+
+#include <iostream>     // std::cout, std::cerr
+#include <fstream>      // std::ifstream
+#include <string>       // std::string, std::to_string
+#include <sstream>      // std::stringstream
+#include <algorithm>    // std::transform
+#include <stdio.h>      // sprintf()
+#include <stdlib.h>     // exit(), EXIT_FAILURE
+#include "cdk.h"
+
+#define MATRIX_WIDTH 3
+#define MATRIX_HEIGHT 5
+#define BOX_WIDTH 20
+#define MATRIX_NAME_STRING "Binary File Contents"
+
+using namespace std;
+
+typedef unsigned char           uint8_t;
+typedef unsigned int            uint32_t;
+typedef unsigned long int       uint64_t;
+
+/* HEADER RECORD FROM BINARY FILE */
+class BinaryFileHeader
+{
+ public:
+  uint32_t magicNumber;               /* Should be 0xFEEDFACE */
+  uint32_t versionNumber;
+  uint64_t numRecords;
+};
+
+/* DATA RECORD FROM BINARY FILE */
+const int maxRecordStringLength = 25;
+
+class BinaryFileRecord
+{
+ public:
+  uint8_t strLength;
+  char stringBuffer[maxRecordStringLength];
+};
+
+/* FUNCTIONS */
+void display(string[MATRIX_HEIGHT + 1][MATRIX_WIDTH + 1]);
+
+#endif /* PROGRAM6_H */
